35240082a13b0676bf4cc01992bd9d1b2b5549af
luiz <luizfelipetomaz30@gmail.com>
2023-09-14 15:29:45 -0300

    ajuste dos comentarios ( responsivo )

--- src/app/components/main/comments/comments.component.ts
+++ src/app/components/main/comments/comments.component.ts
@@ -8,13 +8,13 @@ import { Component } from '@angular/core';
 export class CommentsComponent {
   comments = [
     {
-      avatar: '../../../../assets/Imagens/alcance-global.svg',
+      avatar: '../../../../assets/Imagens/helvys.svg',
       author: 'Helvys',
       comment:
         'O marketing digital permite que você alcance um público global sem fronteiras físicas. Você pode alcançar pessoas de diferentes partes do mundo sem precisar estabelecer presença física em cada local...',
     },
     {
-      avatar: '../../../../assets/Imagens/alcance-global.svg',
+      avatar: '../../../../assets/Imagens/luiz.svg',
       author: 'Luiz',
       comment:
         'O marketing digital permite que você alcance um público global sem fronteiras físicas. Você pode alcançar pessoas de diferentes partes do mundo sem precisar estabelecer presença física em cada local...',
